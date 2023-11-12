1d66912c1674e1aa3d4e85f06df81cf2e914afd3
Jérôme Lanloze <99019668+Jekoff@users.noreply.github.com>
2023-11-12 22:15:49 +0100

    commit

--- script.js
+++ script.js
@@ -2,23 +2,24 @@ const prompt = require("prompt-sync")();
 
 const answer = prompt("Would you like to play a game? ");
 
-if (answer.toLowerCase() === "yes");{
+// Make sure to remove the semicolon after the if statement
+if (answer.toLowerCase() === "yes") {
     const answer2 = prompt("Would you like to go left or right? (left/right)?");
     
-    if (answer2 === "y"){
-        console.log("Oops, you go left and fall of a bridge! You lost");
-}else{
-    // right
+    if (answer2.toLowerCase() === "left") { // Changed "y" to "left" and added toLowerCase()
+        console.log("Oops, you go left and fall off a bridge! You lost");
+    } else {
+        // right
 
-    console.log("Nice You go right and approach a bridge...");
-    const answer3 = prompt("Would you like to cross the bridge, or turn around and find another way (cross/turn?");
+        console.log("Nice! You go right and approach a bridge...");
+        const answer3 = prompt("Would you like to cross the bridge, or turn around and find another way (cross/turn)?");
 
-    if (answer3 === "cross"){
-        console.log("You cross the bridge and reach of the end of the game!");
+        if (answer3.toLowerCase() === "cross") { // Added toLowerCase()
+            console.log("You cross the bridge and reach the end of the game!");
+        } else {
+            console.log("You turn back and trip on a log and hurt your leg. You lost!");
+        }
     }
-}else {
-    console.log("You turn back and the trip on a log and hurt your leg. You lost!") ;
-}
-}else{
-    console.log("That's too bad maybe another time!");
+} else {
+    console.log("That's too bad, maybe another time!");
 }
